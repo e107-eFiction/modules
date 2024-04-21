e9fd8bfc8622e7781fe39bea0512c786f1f39492
Jimako <janamicek@gmail.com>
2024-04-21 18:45:43 +0200

    fix field title

--- storybanner/storyform.php
+++ storybanner/storyform.php
@@ -7,7 +7,7 @@ else {
 }
 
 $output .= 
-"<div style='margin-bottom: 1em'><label for=\"banner\">Banner Tips:</label> 
+"<div style='margin-bottom: 1em'><label for=\"banner\">Banner:</label> 
 <br />
 <input type=\"text\" class=\"textbox\" id=\"storybanner\" maxlength=\"250\" name=\"banner\" size=\"100\"  value=\"" . htmlentities($banner) . "\"></div>";
    
